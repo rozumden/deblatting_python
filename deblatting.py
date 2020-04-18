c2db6d159dc5ddccef656489721925ff00835b3b
Denis Rozumnyi <rozumden@lcnode1.felk.cvut.cz>
2020-04-18 14:17:47 +0200

    estimateFM working

--- deblatting.py
+++ deblatting.py
@@ -10,7 +10,7 @@ from utils import *
 class Params:
 	def __init__(self): ## Parameters with which users can experiment are marked by #!
 		## universal parameters
-		self.loop_maxiter = 10 #! max number of (F,M)/H blind loop alternations  
+		self.loop_maxiter = 100 #! max number of (F,M)/H blind loop alternations  
 		self.maxiter = 10 #! max number of outer iterations
 		self.cg_maxiter = 25 # max number of inner CG iterations ('h' subproblem)
 		self.rel_tol = 2e-3 # relative between iterations difference for outer ADMM loop
@@ -33,10 +33,14 @@ class Params:
 		self.beta_cross_f = 10*self.alpha_cross_f # splitting vc=D_cross*f due to cross-image TV regularizer
 		## visualization parameters 
 		self.verbose = True #!
+		self.do_cost = False #!
+		self.visualize = False #!
 
 
 def estimateFMH(I,B,M=None,F=None,Hmask=None):
 	## Estimate F,M,H in FMO equation I = H*F + (1 - H*M)B, where * is convolution
+	if I.shape != B.shape:
+		raise Exception('Shapes must be equal!')
 	if M is None:
 		M = np.ones(I.shape[:2])
 	if F is None:
@@ -54,7 +58,6 @@ def estimateFMH(I,B,M=None,F=None,Hmask=None):
 	H = np.zeros(I.shape[:2])
 	params = Params()
 	params.maxiter = 1
-	params.verbose = False
 	rel_tol2 = params.rel_tol**2
 	stateh = StateH()
 	statefm = StateFM()
@@ -62,15 +65,16 @@ def estimateFMH(I,B,M=None,F=None,Hmask=None):
 	for iter in range(params.loop_maxiter):
 		H_old = H
 
-		H, stateh = estimateH(I, B, M, F, oHmask=Hmask_small, state=stateh, params=params)
+		H, stateh = estimateH(I, B, M, F, Hmask=Hmask_small, state=stateh, params=params)
 		F, M, statefm = estimateFM(I, B, H, M, F, state=statefm, params=params)
 
 		reldiff2 = np.sum((H_old - H)**2) / np.sum(H**2)
 
-		if True:
-			# imshow(H/np.max(H),wkey=0.5)
-			# imshow(np.r_[np.repeat(M[:,:,np.newaxis], 3, axis=2),F], 0.05, 4)
-			print("FMH: iter={}, reldiff_h={}".format(iter, np.sqrt(reldiff2)))	
+		if params.visualize:
+			imshow(H/np.max(H),wkey=0.5)
+			imshow(np.r_[np.repeat(M[:,:,np.newaxis], 3, axis=2),F], 0.5, 4)
+		if params.verbose:
+			print("FMH: iter={}, reldiff_h={}".format(iter+1, np.sqrt(reldiff2)))	
 
 		if reldiff2 < rel_tol2:
 			break
@@ -79,14 +83,12 @@ def estimateFMH(I,B,M=None,F=None,Hmask=None):
 	He[Hmask] = H[Hmask_small]
 	return He, F, M
 
-def estimateFM(I, B, H, M=None, F=None, F_T=None, M_T=None, oHmask=None, state=None, params=None):
+def estimateFM(I, B, H, M=None, F=None, F_T=None, M_T=None, state=None, params=None):
 	## Estimate F,M in FMO equation I = H*F + (1 - H*M)B, where * is convolution
 	## M is suggested to be specified to know approximate object size, at least as am array of zeros, for speed-up
 	## F_T, M_T - template for F and M
 	if params is None:
 		params = Params()
-	if oHmask is None:
-		Hmask = np.ones(I.shape[:2]).astype(bool)
 	if M is None:
 		if F is not None:
 			M = np.zeros(F.shape[:2])
@@ -217,11 +219,11 @@ def estimateFM(I, B, H, M=None, F=None, F_T=None, M_T=None, oHmask=None, state=N
 		rel_diff2_f = (df @ df)/(ff @ ff)
 		rel_diff2_m = (dm @ dm)/(m @ m)
 		
-		if params.verbose:
-			# f_img = ivec3(f, Fshape); m_img = ivec3(m, Fshape[:2])
-			# imshow(np.r_[np.repeat(m_img[:,:,np.newaxis], 3, axis=2),f_img], 1, 6)
-			# pdb.set_trace()
-			if False: # calculate cost, ot fully implemented for all terms, e.g. lambda_R
+		if params.visualize and params.maxiter > 1:
+			f_img = ivec3(f, Fshape); m_img = ivec3(m, Fshape[:2])
+			imshow(np.r_[np.repeat(m_img[:,:,np.newaxis], 3, axis=2),f_img], 1, 6)
+		if params.verbose and params.maxiter > 1:
+			if params.do_cost: # not fully implemented for all terms, e.g. lambda_R
 				Fe[idx_f,idy_f,idz_f] = ff
 				Me[idx_m,idy_m] = m
 				err = np.sum(np.reshape(np.real(ifft2(fH[:,:,np.newaxis]*fft2(Fe,axes=(0,1)),axes=(0,1)))-B*np.real(ifft2(fH*fft2(Me)))[:,:,np.newaxis]-(I-B), (-1,1))**2)
@@ -249,27 +251,23 @@ def estimateFM(I, B, H, M=None, F=None, F_T=None, M_T=None, oHmask=None, state=N
 		state.vf = vf; state.af = af; state.vm = vm; state.am = am 
 		return f_img,m_img,state
 
-def estimateH(oI, oB, M, F, oHmask=None, state=None, params=None):
+def estimateH(I, B, M, F, Hmask=None, state=None, params=None):
 	## Estimate H in FMO equation I = H*F + (1 - H*M)B, where * is convolution
 	## Hmask represents a region in which computations are done
-	if oI.shape != oB.shape:
-		raise Exception('Shapes must be equal!')
 	if params is None:
 		params = Params()
-	if oHmask is None:
-		Hmask = np.ones(oI.shape[:2]).astype(bool)
-		oHmask = Hmask
-		I = oI
-		B = oB
+	if Hmask is None:
+		Hmask = np.ones(I.shape[:2]).astype(bool)
+		Hmask_small = Hmask
 	else: ## speed-up by padding and ROI
 		if state is None:
 			pads = np.ceil( (np.array(M.shape)-1)/2 ).astype(int)
-			rmin, rmax, cmin, cmax = boundingBox(oHmask, pads)
-			I = oI[rmin:rmax,cmin:cmax,:]
-			B = oB[rmin:rmax,cmin:cmax,:]
-			Hmask = oHmask[rmin:rmax,cmin:cmax]
+			rmin, rmax, cmin, cmax = boundingBox(Hmask, pads)
+			I = I[rmin:rmax,cmin:cmax,:]
+			B = B[rmin:rmax,cmin:cmax,:]
+			Hmask_small = Hmask[rmin:rmax,cmin:cmax]
 		else:
-			I = oI; B = oB; Hmask = oHmask
+			Hmask_small = Hmask
 			
 	H = None
 	if state is None:
@@ -281,9 +279,9 @@ def estimateH(oI, oB, M, F, oHmask=None, state=None, params=None):
 		a_lp = state.a_lp
 
 	if H is None:
-		H = np.zeros((np.count_nonzero(Hmask),))
+		H = np.zeros((np.count_nonzero(Hmask_small),))
 
-	hsize = Hmask.shape
+	hsize = Hmask_small.shape
 
 	iF = fft2(psfshift(F, hsize),axes=(0,1))
 	iM = fft2(psfshift(M, hsize),axes=(0,1))
@@ -293,7 +291,7 @@ def estimateH(oI, oB, M, F, oHmask=None, state=None, params=None):
 
 	## precompute RHS for the 'h' subproblem
 	rhs_const = np.sum(np.real(ifft2(np.conj(iF)*Fgb-np.conj(iM3)*Fbgb,axes=(0,1))),2)
-	rhs_const = params.gamma*rhs_const[Hmask]
+	rhs_const = params.gamma*rhs_const[Hmask_small]
 
 	He = np.zeros(hsize)
 	rel_tol2 = params.rel_tol**2
@@ -315,13 +313,13 @@ def estimateH(oI, oB, M, F, oHmask=None, state=None, params=None):
 		rhs = rhs_const + params.beta_h*(v_lp-a_lp)
 
 		def estimateH_cg_Ax(hfun):
-			He[Hmask] = hfun
+			He[Hmask_small] = hfun
 			FH = fft2(He,axes=(0,1))
 			Fh = iF*np.repeat(FH[:, :, np.newaxis], 3, axis=2) ## apply forward conv (->RGB image, summation over angles)
 			BMh = B*np.repeat(np.real(ifft2(iM*FH,axes=(0,1)))[:, :, np.newaxis], 3, axis=2)
 			Fh_BMh = Fh - fft2(BMh,axes=(0,1))
 			res = np.sum(np.real(ifft2(np.conj(iF)*Fh_BMh - np.conj(iM3)*fft2(B*np.real(ifft2(Fh_BMh,axes=(0,1))),axes=(0,1)),axes=(0,1))),2)
-			res = params.gamma*res[Hmask] + (params.beta_h)*hfun
+			res = params.gamma*res[Hmask_small] + (params.beta_h)*hfun
 			return res
 
 		A = scipy.sparse.linalg.LinearOperator((H.shape[0],H.shape[0]), matvec=estimateH_cg_Ax)
@@ -330,12 +328,12 @@ def estimateH(oI, oB, M, F, oHmask=None, state=None, params=None):
 		Diff = (H - H_old)
 		rel_diff2 = (Diff @ Diff)/(H @ H)
 
-		if params.verbose:
-			if False:
-				He[Hmask] = H
-				He /= np.max(He)
-				imshow(He)
-			if False: ## calculate cost
+		if params.visualize and params.maxiter > 1:
+			He[Hmask_small] = H
+			He /= np.max(He)
+			imshow(He)
+		if params.verbose and params.maxiter > 1:
+			if params.do_cost:
 				FH = fft2(He,axes=(0,1))
 				FH3 = np.repeat(FH[:, :, np.newaxis], 3, axis=2)
 				Fh = iF*FH3
@@ -345,13 +343,11 @@ def estimateH(oI, oB, M, F, oHmask=None, state=None, params=None):
 				print("H: iter={}, reldiff={}, err={}, cost={}".format(iter, np.sqrt(rel_diff2), err, cost))	
 			else:
 				print("H: iter={}, reldiff={}".format(iter, np.sqrt(rel_diff2)))	
-			# pdb.set_trace()
-		
 		if rel_diff2 < rel_tol2:
 			break
 
-	oHe = np.zeros(oHmask.shape)
-	oHe[oHmask] = H
+	oHe = np.zeros(Hmask.shape)
+	oHe[Hmask] = H
 	if state is None:
 		return oHe
 	else:
