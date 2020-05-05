2dee3b01a6949a66b5e069391b9b12f3dccfe41f
Denis Rozumnyi <rozumden@lcgpu2>
2020-05-05 18:40:51 +0200

    gpu version

--- gpu/deblatting_gpu.py
+++ gpu/deblatting_gpu.py
@@ -6,6 +6,7 @@ from scipy import sparse
 from utils import *
 from vis import *
 from deblatting import *
+from gpu.torch_cg import *
 
 import torch
 
@@ -20,29 +21,25 @@ def estimateH_gpu(Ic, Bc, Mc, Fc, params=None):
 	B = torch.from_numpy(Bc[:,:,:,np.newaxis]).float().to(device).permute([3,2,0,1])
 	M = torch.from_numpy(Mc[:,:,np.newaxis,np.newaxis]).float().to(device).permute([3,2,0,1])
 	F = torch.from_numpy(Fc[:,:,:,np.newaxis]).float().to(device).permute([3,2,0,1])
+	BS = I.shape[0]
 
-	nx = I.shape[2]*I.shape[3]
-
-	v_lp = 0
-	a_lp = 0 
-	H = torch.zeros((nx,))
+	H = torch.zeros((BS,1,I.shape[2],I.shape[3])).float().to(device)
+	v_lp = torch.zeros_like(H).float().to(device)
+	a_lp = torch.zeros_like(H).float().to(device)
+	rgnA = torch.arange(1,I.shape[2]*I.shape[3]+1).float().to(device)
 	hsize = I.shape[2:]
 	
 	iF = torch.rfft(psfshift_gpu(F, hsize, device), signal_ndim=2, normalized=False, onesided=False)
-	iFconj = torch.cat([iF[:,:,:,:,:1],-iF[:,:,:,:,1:]], dim=4)
+	iFconj = complex_conj(iF)
 	iM = torch.rfft(psfshift_gpu(M, hsize, device), signal_ndim=2, normalized=False, onesided=False)
-	iMconj = torch.cat([iM[:,:,:,:,:1],-iM[:,:,:,:,1:]], dim=4)
+	iMconj = complex_conj(iM)
 	Fgb = torch.rfft(I-B, signal_ndim=2, normalized=False, onesided=False)
 	Fbgb = torch.rfft(B*(I-B), signal_ndim=2, normalized=False, onesided=False)
 
-	pdb.set_trace()
-
 	## precompute RHS for the 'h' subproblem
-	term = iFconj*Fgb - iMconj*Fbgb
-	rhs_const = np.sum(np.real(ifft2(term,axes=(0,1))),2)
-	rhs_const = params.gamma*rhs_const[Hmask_small]
+	term = complex_multiplication(iFconj,Fgb) - complex_multiplication(iMconj, Fbgb)
+	rhs_const = params.gamma*torch.irfft(term, signal_ndim=2, normalized=False, onesided=False).sum(1)
 
-	He = np.zeros(hsize)
 	rel_tol2 = params.rel_tol_h**2
 	## ADMM loop
 	for iter in range(params.maxiter):
@@ -50,41 +47,35 @@ def estimateH_gpu(Ic, Bc, Mc, Fc, params=None):
 		if params.beta_h > 0: ## also forces positivity
 			v_lp = H + a_lp
 			if params.sum1:
-				v_lp = proj2simplex(v_lp)
-			elif params.lp == 1:
-				temp = v_lp < params.alpha_h/params.beta_h
-				v_lp[temp] = 0 
-				v_lp[~temp] -= (params.alpha_h/params.beta_h)
-			elif params.lp == 0:
-				v_lp[v_lp <= np.sqrt(2*params.alpha_h/params.beta_h)] = 0
+				v_lp = proj2simplex_gpu(v_lp, rgnA)
 			a_lp = a_lp + H - v_lp
 
 		rhs = rhs_const + params.beta_h*(v_lp-a_lp)
 
-		def estimateH_cg_Ax(hfun):
-			He[Hmask_small] = hfun
-			FH = fft2(He,axes=(0,1))
-			Fh = iF*np.repeat(FH[:, :, np.newaxis], 3, axis=2) ## apply forward conv (->RGB image, summation over angles)
-			BMh = B*np.repeat(np.real(ifft2(iM*FH,axes=(0,1)))[:, :, np.newaxis], 3, axis=2)
-			Fh_BMh = Fh - fft2(BMh,axes=(0,1))
-			res = np.sum(np.real(ifft2(np.conj(iF)*Fh_BMh - np.conj(iM3)*fft2(B*np.real(ifft2(Fh_BMh,axes=(0,1))),axes=(0,1)),axes=(0,1))),2)
-			res = params.gamma*res[Hmask_small] + (params.beta_h)*hfun
+		def A_bmm(hfun):
+			FH = torch.rfft(hfun, signal_ndim=2, normalized=False, onesided=False)
+			Fh = complex_multiplication(iF, FH)
+			BMh = B*torch.irfft(complex_multiplication(iM, FH),signal_ndim=2, normalized=False, onesided=False)
+			Fh_BMh = Fh - torch.rfft(BMh, signal_ndim=2, normalized=False, onesided=False)
+			term = complex_multiplication(iFconj,Fh_BMh) - iMconj*torch.rfft( B*torch.irfft(Fh_BMh,signal_ndim=2, normalized=False, onesided=False),signal_ndim=2, normalized=False, onesided=False)
+			res = torch.irfft(term,signal_ndim=2, normalized=False, onesided=False).sum(1)
+			res = params.gamma*res.unsqueeze(1) + params.beta_h*hfun
 			return res
 
-		A = scipy.sparse.linalg.LinearOperator((H.shape[0],H.shape[0]), matvec=estimateH_cg_Ax)
-		H, info = scipy.sparse.linalg.cg(A, rhs, H, params.cg_tol, params.cg_maxiter)
+		pdb.set_trace()
 
-		if state is not None:
-			continue
+		# X = CG(A_bmm, H)(rhs)
+		H, info = cg_batch(A_bmm, rhs, X0=H, rtol=params.cg_tol, maxiter=params.cg_maxiter)
+		# H, info = scipy.sparse.linalg.cg(A, rhs, H, params.cg_tol, params.cg_maxiter)
 
 		Diff = (H - H_old)
 		rel_diff2 = (Diff @ Diff)/(H @ H)
 
-		if params.visualize:
-			imshow_nodestroy(get_visim(He,F,M,I), 600/np.max(I.shape))
+		if params.visualize and False:
+			imshow_nodestroy(get_visim(H,F,M,I), 600/np.max(I.shape))
 		if params.verbose:
 			if params.do_cost:
-				FH = fft2(He,axes=(0,1))
+				FH = fft2(H,axes=(0,1))
 				FH3 = np.repeat(FH[:, :, np.newaxis], 3, axis=2)
 				Fh = iF*FH3
 				BMh = B*np.real(ifft2(iM3*FH3,axes=(0,1)));
@@ -99,7 +90,27 @@ def estimateH_gpu(Ic, Bc, Mc, Fc, params=None):
 	oHe = np.zeros(Hmask.shape)
 	oHe[Hmask] = H
 	return oHe
-	
+
+def proj2simplex_gpu(Y, rgnA):
+	## euclidean projection of a batch of y to a simplex defined as x>=0 and sum(x(:)) = 1
+	Yf = torch.reshape(Y, (Y.shape[0],-1))
+	X = torch.sort(Yf, dim=-1, descending=True)[0]
+	temp = (torch.cumsum(X, dim=-1)-1)/rgnA
+	inds = torch.argmax( (X > temp).float()*rgnA ,dim=1)
+	X = torch.reshape(Yf - temp[(torch.arange(0,X.shape[0]),inds)].unsqueeze(1), Y.shape)
+	X[X < 0] = 0
+	return X
+
+def complex_multiplication(arr1, arr2):
+	re1 = arr1[:,:,:,:,0]
+	im1 = arr1[:,:,:,:,1]
+	re2 = arr2[:,:,:,:,0]
+	im2 = arr2[:,:,:,:,1]
+	return torch.stack([re1 * re2 - im1 * im2, re1 * im2 + im1 * re2], dim = -1)
+
+def complex_conj(arr):
+	return torch.stack([arr[:,:,:,:,0],-arr[:,:,:,:,1]], dim=-1)
+
 def psfshift_gpu(H, usize, device):
 	## PSFSHIFT Moves PSF center to origin and extends the PSF to be the same size as image (for use with FT). 
 	## ipsfshift_gpu does the reverse. GPU torch version
