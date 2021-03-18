7ab66d864a2e988e7007dcae8363efb31d41c448
Denys Rozumnyi <denysr@lo-login-01.leonhard.ethz.ch>
2021-03-18 15:05:23 +0100

    update

---
+++ deblatting_runners.py
@@ -0,0 +1,63 @@
+import numpy as np
+from gpu.deblatting_gpu import *
+from deblatting_pw import *
+
+def deblatting_oracle_runner(img,bgrr,debl_dim,gt_traj):
+	nsplits = gt_traj.shape[1]
+	Hso = np.zeros(img.shape[:2]+(nsplits,))
+
+	for ni in range(nsplits): 
+		if ni < nsplits-1:
+			pars = np.array([gt_traj[:,ni], gt_traj[:,ni+1]-gt_traj[:,ni]]).T
+		else:
+			pars = np.array([gt_traj[:,ni], gt_traj[:,ni]-gt_traj[:,ni-1]]).T
+		Hso[:,:,ni] = renderTraj(pars, Hso[:,:,ni])
+	Hso /= Hso.sum()
+
+	Fs,Ms = estimateFM_pw(img,bgrr,Hso,np.zeros(tuple(debl_dim)+(nsplits,)))
+	rgba_tbd3d = np.concatenate((Fs, Ms),2)
+	return rgba_tbd3d, Hso
+
+def deblatting_single_runner(imr,bgrr,nsplits,debl_dim):
+	dI = imr.transpose(2,0,1)[np.newaxis,:,:,:]
+	dB = bgrr.transpose(2,0,1)[np.newaxis,:,:,:]
+	M0 = np.zeros(debl_dim)
+	H,F,M = estimateFMH_gpu(dI, dB, M0)
+	return H,F,M
+
+def deblatting_runner(imr,bgrr,nsplits,debl_dim):
+	dI = imr.transpose(2,0,1)[np.newaxis,:,:,:]
+	dB = bgrr.transpose(2,0,1)[np.newaxis,:,:,:]
+	M0 = np.zeros(debl_dim)
+	H,F,M = estimateFMH_gpu(dI, dB, M0)
+	Fc = F.cpu().numpy()[0].transpose(1,2,0)
+	Mc = M.cpu().numpy()[0].transpose(1,2,0)
+	Hc = H.cpu().numpy()[0,0]
+	Hc /= Hc.sum()
+	Hf,pars = psffit(Hc,True)
+	Fc,Mc = estimateFM(imr,bgrr,Hf,Mc[:,:,0])
+	mynorm = np.linalg.norm(pars[:,1])
+	if mynorm < 1:
+		red_nsplits = 1
+	else:
+		pcd = nsplits
+		while pcd % 2 == 0 and pcd > 0: pcd = pcd // 2 
+		red_nsplits = pcd*int(np.min([nsplits/pcd, np.max([1,2**int(np.log2(mynorm))])]))
+	Hs = psfsplit(Hc,red_nsplits)
+	Fs,Ms = estimateFM_pw(imr,bgrr,Hs,np.zeros(tuple(debl_dim)+(red_nsplits,)))
+	inds = np.repeat(range(red_nsplits), int(nsplits/red_nsplits))
+	Hs = Hs[:,:,inds]
+	Fs = Fs[:,:,:,inds]
+	Ms = Ms[:,:,:,inds]
+	est_hs_tbd = np.zeros(imr.shape+(nsplits,))
+	est_hs_tbd3d = np.zeros(imr.shape+(nsplits,))
+	est_traj = np.zeros((2,nsplits))
+	timestamps = np.linspace(0,1,nsplits)
+	for ki in range(nsplits): 
+		Hsc = Hs[:,:,ki]/np.sum(Hs[:,:,ki])
+		est_hs_tbd[:,:,:,ki] = fmo_model(bgrr,Hsc,Fc,Mc)
+		est_hs_tbd3d[:,:,:,ki] = fmo_model(bgrr,Hsc,Fs[:,:,:,ki],Ms[:,:,0,ki])
+		est_traj[:,ki] = pars[:,0] + timestamps[ki]*pars[:,1]
+	rgba_tbd = np.repeat(np.concatenate((Fc, Mc[:,:,None]),2)[...,None], nsplits, 3)
+	rgba_tbd3d = np.concatenate((Fs, Ms),2)
+	return est_hs_tbd, est_hs_tbd3d, rgba_tbd, rgba_tbd3d, est_traj[[1,0]], Hs
